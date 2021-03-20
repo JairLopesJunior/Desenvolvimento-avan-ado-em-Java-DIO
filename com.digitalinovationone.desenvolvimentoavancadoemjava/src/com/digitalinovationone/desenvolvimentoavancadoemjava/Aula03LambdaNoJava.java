331de545e44efc4b530d65bd8df8bfb52c20df3c
JairLopesJunior <jair.lopes@fatec.sp.gov.br>
2021-03-20 15:08:53 -0300

    Lambda no Java - Interfaces Funcionais

--- com.digitalinovationone.desenvolvimentoavancadoemjava/src/com/digitalinovationone/desenvolvimentoavancadoemjava/Aula03LambdaNoJava.java
+++ com.digitalinovationone.desenvolvimentoavancadoemjava/src/com/digitalinovationone/desenvolvimentoavancadoemjava/Aula03LambdaNoJava.java
@@ -5,8 +5,14 @@ public class Aula03LambdaNoJava {
 	public static void main(String[] args) {
 		//Funcao f = valor -> valor;
 
-		Funcao colocarPrefixoSenhorNaString = valor -> "Sr. " + valor;
+		Funcao colocarPrefixoSenhorNaString = valor -> "Sr. " + valor; // Se ter somente uma instrução não é necessario usar chaves
 		System.out.println(colocarPrefixoSenhorNaString.gerar("Jair"));
+		Funcao colocarPrefixoSenhorNaString2 = valor2 -> {
+			String nomeComSenhor = "Sr. " + valor2;
+			String nomeComSenhorEPontoFinal = nomeComSenhor + ".";
+			return nomeComSenhorEPontoFinal;
+		};
+		System.out.println(colocarPrefixoSenhorNaString2.gerar("Joao"));
 	}
 
 }
