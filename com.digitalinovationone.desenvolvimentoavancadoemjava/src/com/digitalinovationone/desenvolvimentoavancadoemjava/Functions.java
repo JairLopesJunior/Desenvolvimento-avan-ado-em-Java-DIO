6067d6fc8c1358fc2200c8dc95b1115710788cdd
JairLopesJunior <jair.lopes@fatec.sp.gov.br>
2021-03-23 00:02:34 -0300

    Apendendo sobre a Interface Function

---
+++ com.digitalinovationone.desenvolvimentoavancadoemjava/src/com/digitalinovationone/desenvolvimentoavancadoemjava/Functions.java
@@ -0,0 +1,15 @@
+package com.digitalinovationone.desenvolvimentoavancadoemjava;
+
+import java.util.function.Function;
+
+public class Functions {
+
+	public static void main(String[] args) {
+		Function<String, String> retornarNomeAoContrario = texto -> new StringBuilder(texto).reverse().toString();
+		Function<String,Integer> converterStringParaInteiro = string -> Integer.valueOf(string);
+		System.out.println(retornarNomeAoContrario.apply("Jair")); // Retornando a String ao contrario
+		System.out.println(converterStringParaInteiro.apply("23452")); // Retornando a String em Inteiro
+
+	}
+
+}
