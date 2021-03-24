67c628c07bd88065f49b7c9a80558705d51f30ed
JairLopesJunior <jair.lopes@fatec.sp.gov.br>
2021-03-23 23:14:33 -0300

    Entendendo melhor Stream.of() do lambda no JAVA

---
+++ com.digitalinovationone.desenvolvimentoavancadoemjava/src/com/digitalinovationone/desenvolvimentoavancadoemjava/Streams.java
@@ -0,0 +1,26 @@
+package com.digitalinovationone.desenvolvimentoavancadoemjava;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Streams {
+
+	public static void main(String[] args) {
+		String[] nomes = {"Jair", "Paulo", "Oliveira", "Santos", "Instrutor", "Java"};
+		Integer[] numeros = {1, 2, 3, 4, 5};
+		imprimirNomesFiltrados(nomes);
+	}
+	
+	private static void imprimirNomesFiltrados(String... nomes) {
+		String nomesParaImprimir = Stream.of(nomes)
+							.filter(f -> f.equals("Jair"))
+							.collect(Collectors.joining()); // Ele pega cada valor do Array de String
+		
+		System.out.println(nomesParaImprimir);
+		
+		Stream.of(nomes)
+			//.forEach(System.out::println); // Existe este jeito e o de bbaixo
+			.forEach(paraCada -> System.out.println(paraCada));
+	}
+
+}
