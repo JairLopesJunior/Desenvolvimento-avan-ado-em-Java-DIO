123664389cfc2045ef9b9a32b8737320cbf342c0
JairLopesJunior <jair.lopes@fatec.sp.gov.br>
2021-03-24 00:08:15 -0300

    Concluindo o conceito de Stream em JAVA

--- com.digitalinovationone.desenvolvimentoavancadoemjava/src/com/digitalinovationone/desenvolvimentoavancadoemjava/Streams.java
+++ com.digitalinovationone.desenvolvimentoavancadoemjava/src/com/digitalinovationone/desenvolvimentoavancadoemjava/Streams.java
@@ -9,6 +9,7 @@ public class Streams {
 		String[] nomes = {"Jair", "Paulo", "Oliveira", "Santos", "Instrutor", "Java"};
 		Integer[] numeros = {1, 2, 3, 4, 5};
 		imprimirNomesFiltrados(nomes);
+		imprimirODobroDeCadaItemDaLista(numeros);
 	}
 	
 	private static void imprimirNomesFiltrados(String... nomes) {
@@ -21,6 +22,11 @@ public class Streams {
 		Stream.of(nomes)
 			//.forEach(System.out::println); // Existe este jeito e o de bbaixo
 			.forEach(paraCada -> System.out.println(paraCada));
+	}
+	
+	public static void imprimirODobroDeCadaItemDaLista(Integer... numeros) {
+		Stream.of(numeros)
+		.forEach(n -> System.out.println(n*2));
 	}
 
 }
